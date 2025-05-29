3817d049a9450f67891da0fd39b556cd6ef30049
champion316 <royer.seguracalderon@gmail.com>
2025-05-29 16:01:38 +0200

    Create booking form component

---
+++ frontend/src/components/BookingForm.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Form, Card, Button } from 'react-bootstrap';
+import '../styles/BookingForm.css';
+
+function BookingForm() {
+  return (
+    <Card className="booking-form-card">
+      <Card.Body>
+        <Form>
+          <div className="row">
+            <div className="col-4">
+              <Form.Group className="mb-3" controlId="date">
+                <Form.Label>Date</Form.Label>
+                <Form.Control type="date" />
+              </Form.Group>
+            </div>
+            <div className="col-4">
+              <Form.Group className="mb-3" controlId="time">
+                <Form.Label>Time</Form.Label>
+                <Form.Select>
+                  <option value="" disabled>Select a time</option>
+                  <option value="4:00 PM">4:00 PM</option>
+                  <option value="6:00 PM">6:00 PM</option>
+                  <option value="8:00 PM">8:00 PM</option>
+                  <option value="10:00 PM">10:00 PM</option>
+                </Form.Select>
+              </Form.Group>
+            </div>
+            <div className="col-4">
+              <Form.Group className="mb-3" controlId="guests">
+                <Form.Label>Number of Guests</Form.Label>
+                <Form.Control type="number" min="2" max="12" />
+              </Form.Group>
+            </div>
+          </div>
+
+          <Form.Group className="mb-3" controlId="wholeRestaurant">
+            <Form.Check type="checkbox" label="Reserve Whole Restaurant" />
+          </Form.Group>
+
+          <Button variant="primary" className="w-100">
+            Book Now
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default BookingForm;
