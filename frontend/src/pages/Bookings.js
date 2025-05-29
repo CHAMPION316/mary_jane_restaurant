849065d341d03a59adbeb024caf12c0855b7db44
champion316 <royer.seguracalderon@gmail.com>
2025-05-29 16:03:38 +0200

    Create html structure for Bookings.js

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import BookingForm from '../components/BookingForm';
 
 const Bookings = () => {
   return (
     <div className="container mt-5">
       <h1>Bookings</h1>
       <p>Make a reservation at The Mary Jane.</p>
+      <BookingForm />
     </div>
   );
 };
