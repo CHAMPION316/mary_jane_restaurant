e08ad30de35fb94031f65fbeca6254a7b1e84cf8
champion316 <royer.seguracalderon@gmail.com>
2026-06-08 23:35:09 +0200

    add image update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import LoginBackground from '../assets/images/wine-login.jpg';
 
 const Login = () => {
   return (
-    <div className="container mt-5">
-      <h1>Login</h1>
-      <p>Sign in to manage your bookings.</p>
+    <div 
+    style={{
+      backgroundImage: `url(${LoginBackground})`,
+      backgroundAttachment: 'fixed',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      minHeight: '100vh',
+      width: '100%',
+    }}>
+
+      <div className="container mt-5">
+        <h1>Login</h1>
+        <p>Sign in to manage your bookings.</p>
+      </div>
     </div>
   );
 };  
